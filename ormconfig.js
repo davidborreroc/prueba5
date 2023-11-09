c1072559f43830370fdb9d20736bb59eadbb64a5
davidborreroc <149751650+davidborreroc@users.noreply.github.com>
2023-11-08 20:08:24 -0500

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
     type: 'postgres',
-    host:  process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host:  process.env.RDS_HOSTNAME,
+    port:  process.env.RDS_PORT,
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
     entities: ['dist/**/*.entity{.ts,.js}'],
     migrations: [/*...*/],
     synchronize: true, // Solo para entornos de desarrollo. No usar en producción.
